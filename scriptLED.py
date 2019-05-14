4767b9f5b906a203d67f9f140dc882ff50063dd3
Min <msl052@ucsd.edu>
2019-05-14 12:05:48 -0700

    reset

---
+++ scriptLED.py
@@ -0,0 +1,29 @@
+import requests
+
+# import the RGB and Color libraries
+from libraries.RGB_LED import RGB_LED
+from libraries.RGB_LED.RGB_LED.Color import Color
+
+# the address we will make the request to
+# REPLACE WITH YOUR URL
+settingUrl='http://intro-to-iot-lesson.herokuapp.com/api/settings'
+
+# construct the RGB LED and Color object
+red_pin = 22
+green_pin = 27
+blue_pin = 17
+is_common_anode = True
+rgb = RGB_LED.RGB_LED(red_pin,green_pin,blue_pin,is_common_anode)
+color = Color()
+
+while True:
+
+# make a get request to retrieve the current settings, and extract the JSON from it
+r = requests.get(settingUrl)
+print(r)
+
+# set the color to the lightColor field of the settings object
+color.set_color_string(r)
+
+# set the RGB's color
+rgb.set_color(color)
---
+++ scriptName.py
@@ -0,0 +1,34 @@
+import requests
+# import the DHT and MCP libraries
+from libraries.DHT22 import DHT22
+from libraries.MCP300X import MCP300X
+
+
+# construct the DHT22
+dht22_pin = 16
+dht22 = DHT22(dht22_pin)
+# construct the ldr
+mcp3004 = MCP300X.MCP3004
+ldr = MCP300X(mcp3004)
+
+# the address we will make the request to
+# REPLACE WITH YOUR URL
+url='http://intro-to-iot-lesson.herokuapp.com/api/data'
+
+# initialize an empty dictionary
+packet = {}
+
+# get the temperature and humidity from DHT object
+humidity, temperature = dht22.get_temperature_and_humidity()
+
+# get the brightness from the ldr object
+brightness = ldr.read(ldr.CH2)
+
+# fill the packet with data in the format expected by the web API
+packet['temperature'] = round(temperature, 3)
+
+# just a debug, comment it out when you know the script works
+print(packet)
+
+# submit the post request.
+r = requests.post(url,json=packet)
